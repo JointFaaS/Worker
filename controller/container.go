bf6f9b2fa6a51f157e7ac8019f02718b4b236180
Veiasai <o206@qq.com>
2020-01-28 12:12:36 +0800

    namedpipe

--- controller/container.go
+++ controller/container.go
@@ -7,6 +7,7 @@ import (
 	"path"
 	
 	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/mount"
 )
 // ContainerMeta defines the metadata of a container
@@ -16,11 +17,23 @@ type ContainerMeta struct {
 	sequenceID int32
 }
 
-func (c *Client) createContainer(ctx context.Context, name string, image string, sequence string) (container.ContainerCreateCreatedBody, error) {
-	os.Mkdir(path.Join("/tmp", name), 0777)
-	os.Mkdir(path.Join("/tmp", name, sequence), 0777)
-	syscall.Mkfifo(path.Join("/tmp", name, sequence, "down") , 0666)
-	syscall.Mkfifo(path.Join("/tmp", name, sequence, "up") , 0666)
+func prepareNamedPipeForContainer(name string) {
+	err := os.Mkdir(path.Join("/tmp", name), 0777)
+	if err != nil {
+		panic(err)
+	}
+	err = syscall.Mkfifo(path.Join("/tmp", name, "down") , 0666)
+	if err != nil {
+		panic(err)
+	}
+	err = syscall.Mkfifo(path.Join("/tmp", name, "up") , 0666)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (c *Client) createContainer(ctx context.Context, containerName string, image string) (container.ContainerCreateCreatedBody, error) {
+	prepareNamedPipeForContainer(containerName)
 
 	body, err := c.dockerClient.ContainerCreate(ctx, 
 		&container.Config{
@@ -30,24 +43,61 @@ func (c *Client) createContainer(ctx context.Context, name string, image string,
 			Mounts: []mount.Mount{
 				mount.Mount{
 					Type: mount.TypeNamedPipe,
-					Source: path.Join("/tmp", name, "up"),
+					Source: path.Join("/tmp", containerName, "up"),
 					Target: "/up",
 				},
 				mount.Mount{
 					Type: mount.TypeNamedPipe,
-					Source: path.Join("/tmp", name, "down"),
+					Source: path.Join("/tmp", containerName, "down"),
 					Target: "/down",
 				},
 			},
 			NetworkMode: "none",
 		},
-		nil, name)
+		nil, containerName)
 	return body, err
 }
 
-// GetNamedPipeOfEnv returns the up and down pipes for a running container
-func (c *Client) getNamedPipeOfContainer(containerName string) (*os.File, *os.File, error){
-	up, _ := os.OpenFile(path.Join("/tmp", containerName, "up"), os.O_RDWR, os.ModeNamedPipe)
-	down, _ := os.OpenFile(path.Join("/tmp", containerName, "down"), os.O_RDWR, os.ModeNamedPipe)
-	return up, down, nil
+func (c *Client) clearContainer(ctx context.Context) (error) {
+	containers, err := c.dockerClient.ContainerList(
+		ctx, 
+		types.ContainerListOptions{
+			All: true,
+		})
+	if err != nil {
+		return err
+	}
+	for _, ct := range containers {
+		err = c.dockerClient.ContainerRemove(ctx, ct.ID, types.ContainerRemoveOptions{Force: true})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
+
+type containerPipe struct {
+	up *os.File
+	down *os.File
+}
+
+// GetNamedPipeOfEnv returns the up and down pipes for a running container
+func (c *Client) getNamedPipeOfContainer(containerName string) (*containerPipe, error){
+	pipes, isPresent := c.containerPipeMap[containerName]
+	if isPresent {
+		return pipes, nil
+	}
+	up, err := os.OpenFile(path.Join("/tmp", containerName, "up"), os.O_RDWR, os.ModeNamedPipe)
+	if err != nil {
+		return nil, err
+	}
+	down, err := os.OpenFile(path.Join("/tmp", containerName, "down"), os.O_RDWR, os.ModeNamedPipe)
+	if err != nil {
+		up.Close()
+		return nil, err
+	}
+	pipes = &containerPipe{up: up, down: down}
+	c.containerPipeMap[containerName] = pipes
+	return pipes, nil
+}
+
